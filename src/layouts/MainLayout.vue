6bde333455027ab18593c5fc90c943f8abe777e6
Bhramarambha8660 <bhramarambhacb8660@gmail.com>
2024-12-28 10:42:31 +0530

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,99 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        
+
+        <q-toolbar-title>
+
+        <q-img src="logo.png"
+        style="width: 120px;"></q-img>
+          
+        </q-toolbar-title>
+
+        <div class="gt-sm">
+          <q-btn outline  flat color="white" class="q-mr-sm" label="COURSES" />
+          <q-btn outline rounded color="white" class="q-mr-sm" label="SIGN UP" />
+          <q-btn outline rounded color="white" label="SIGN IN" />
+          </div>
+
+          <div class="lt-md">
+            <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+          </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in linksList"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated>
+        <q-toolbar>
+        <q-space />
+        &copy; Innorium Business Solutions 2025.
+        <q-space />
+        </q-toolbar>
+      </q-footer>
+
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'SIGN IN',
+    caption: 'sign in here..',
+    // icon: 'school',
+    // link: 'https://quasar.dev'
+  },
+  {
+    title: 'SIGN UP',
+    caption: 'sing up here',
+    // icon: 'code',
+    // link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'COURSES',
+    caption: 'course details',
+    // icon: 'chat',
+    // link: 'https://chat.quasar.dev'
+  },
+  
+]
+
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer () {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,187 @@
+<template>
+  <q-page class="bg-deep-purple-1" >
+
+  <div class="row bg-primary">
+    <div class="col ">
+    <div style="max-width: 650px; margin-left: auto; margin-right: auto; text-align: center;">
+  <div class="text-h4 text-bold text-white q-mt-xl q-pt-xl q-mb-lg"> Learn fullstack development from IT Experts</div>
+  <div  class=" text-h6 text-white q-pl-lg q-pr-lg q-pb-sm">join the code expoert youtube channel to increase your coding skill</div>
+
+  <q-input
+        v-model="search"
+        color="black"
+        bg-color="white"
+        filled
+        placeholder="Your Email.."
+        class="q-pl-lg q-pr-lg q-pb-lg"
+      >
+        <template v-slot:prepend>
+          <q-icon name="email" />
+        </template>
+
+        <template v-slot:append>
+          <q-btn color="purple">Start Learning</q-btn>
+        </template>
+      </q-input>
+    </div>
+  </div>
+
+  <div class="col gt-sm">
+  <q-img src="quasarlogo.png"
+  style="margin-left: auto; margin-right: auto; display:block"
+></q-img>
+  </div>
+</div>
+
+<div class="row">
+<div class="text-h5 text-bold q-px-md q-pt-sm">Latest Session</div>
+</div>
+<div class="row">
+<div class="col col-12 col-md-4 col-sm-6">
+  <div class="q-pa-md">
+    <q-card class="my-card">
+      <q-video src="https://www.youtube.com/embed/IlU-zDU6aQ0?rel=0" />
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </q-card-section>
+    </q-card>
+  </div>
+</div>
+
+
+<div class="col col-12 col-md-4 col-sm-6">
+  <div class="q-pa-md">
+    <q-card class="my-card">
+      <q-video src="https://www.youtube.com/embed/dQw4w9WgXcQ?rel=0" />
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </q-card-section>
+    </q-card>
+  </div>
+</div>
+
+
+
+<div class="col col-12 col-md-4 col-sm-6">
+  <div class="q-pa-md">
+    <q-card class="my-card">
+      <q-video src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0"  />
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </q-card-section>
+    </q-card>
+  </div>
+</div>
+
+
+<div class="col col-12 col-md-4 col-sm-6">
+  <div class="q-pa-md">
+    <q-card class="my-card">
+      <q-video src="https://www.youtube.com/embed/UAhRf3U50lM?rel=0"  />
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </q-card-section>
+    </q-card>
+  </div>
+</div>
+
+
+<div class="col col-12 col-md-4 col-sm-6">
+  <div class="q-pa-md">
+    <q-card class="my-card">
+      <q-video src="https://www.youtube.com/embed/mNBmG24djoY?rel=0"  />
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </q-card-section>
+    </q-card>
+  </div>
+</div>
+
+<div class="col col-12 col-md-4 col-sm-6">
+  <div class="q-pa-md">
+    <q-card class="my-card">
+      <q-video src="https://www.youtube.com/embed/IlU-zDU6aQ0?rel=0" />
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </q-card-section>
+    </q-card>
+  </div>
+</div>
+
+
+</div>
+
+
+<div class="row">
+<div class="text-h5 text-bold q-pl-md">Featured Lession...</div>
+</div>
+
+<div class="row">
+<div class="col-md col-12">
+  <div class="q-pa-md">
+    <q-card class="my-card">
+
+    <div class="row">
+    <div class="col-12 col-md-4 q-pa-md">
+    <q-img src="card.jpg" />
+    </div>
+    <div class="col-12 col-md-8">
+    <div class="text-h5 text-bold q-pl-md q-pt-sm text-purple" >Getting startted with quasar Fram work in vue JS</div>
+    <div class="q-pa-md text-sm text-blue-grey-10 text-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem excepturi nulla sit iure temporibus ex eos debitis ratione eligendi obcaecati optio facere vitae ad asperiores, alias provident delectus exercitationem. Nostrum.</div>
+    </div>
+    </div>
+
+ 
+    </q-card>
+  </div>
+</div>
+</div>
+  
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
